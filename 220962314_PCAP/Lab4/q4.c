48621864cff7ea93c50deee3677ae57339c38c31
morvinp <123289878+morvinp@users.noreply.github.com>
2025-02-01 12:37:57 +0530

    Add files via upload

--- 220962314_PCAP/Lab4/q4.c
+++ 220962314_PCAP/Lab4/q4.c
@@ -15,35 +15,37 @@ int main(int argc,char* argv[]){
 	}
 	char val[1];
 	MPI_Scatter(str,1,MPI_CHAR,val,1,MPI_CHAR,0,MPI_COMM_WORLD);
-	// MPI_Scan(factans,result,4,MPI_INT,MPI_SUM,MPI_COMM_WORLD);
-	// MPI_Gather(result,4,MPI_INT,r,4,MPI_INT,0,MPI_COMM_WORLD);
-	// MPI_Reduce(&factans,&factsum,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
 	int len = rank+1;
-	// printf("%s\t at rank %d",val,rank);
-	char ans[rank+2];
+	char ans[len+1];
 	for(int i = 0;i<rank+1;i++){
 		// printf("%s\n",ans);
 		ans[i]=val[0];
 	
 	}
+	ans[len]='\0';
 	// printf("%s is the ans", ans);
-	char result[100];
 	for(int i=0;i<size;i++){
 		// MPI_Send(&len,1,MPI_INT,i,i,MPI_COMM_WORLD);
 		// printf("I am sending %s at %d\n",ans,rank);
 		MPI_Send(&len,1,MPI_INT,0,i,MPI_COMM_WORLD);
-		MPI_Send(ans,rank+1,MPI_CHAR,0,i,MPI_COMM_WORLD);
+		MPI_Send(ans,len,MPI_CHAR,0,i,MPI_COMM_WORLD);
 
 	}
 	char res[100];
 	int lens;
 	if(rank ==0){
+		char result[100] = "";
+
 		for(int i = 0;i<size;i++){
 			MPI_Recv(&lens,1,MPI_INT,i,i,MPI_COMM_WORLD,&stat);
-			MPI_Recv(res,len,MPI_CHAR,i,i,MPI_COMM_WORLD,&stat);
+			printf("%d is the value of lens\n",lens);
+			MPI_Recv(res,lens,MPI_CHAR,i,i,MPI_COMM_WORLD,&stat);
 			// printf("i am getting %s at rank %d",res,rank);
 			// strcat(result,res);
+			res[lens]='\0';
 			printf("Answer is %s\n",res);
+			strcat(result,res);
 		}
 		printf("%s is the answer hopefully", result);
 	}
